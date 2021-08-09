f4840722b4c769641f5fe8729aa1ae8a797c1e6c
Thomas Winter <thomaswinter2@gmail.com>
2021-08-09 09:24:10 -0400

    addLeague subroutine

--- backend/routes/leagues.js
+++ backend/routes/leagues.js
@@ -7,19 +7,15 @@ var async = require('async');
 const {Team} = require('../models/team');
 const {Player} = require('../models/player');
 
-
-router.route('/').get((req, res) => {
-  League.find()
-    .then(leagues => res.json(leagues))
-    .catch(err => res.status(400).json('error: ' + err));
-});
-
-router.route('/add').post((req, res) => {
-  let name = req.body.name;
-  let abbreviation = req.body.abbreviation;
-  let teams = req.body.players;
-
+async function addLeague(league){
+  let id = league.id;
+  let name = league.name;
+  let abbreviation = league.abbreviation;
+  let shortName = league.shortName;
+  let slug = league.slug;
+  let teams = [];
   const newLeague = new League({
+    _id: new mongoose.Types.ObjectId(),
     id,
     name,
     abbreviation,
@@ -27,23 +23,137 @@ router.route('/add').post((req, res) => {
     slug,
     teams,
   });
+  async.each(league.teams, async (teamData) => {
+    let id = teamData.team.id;
+    let uid = teamData.team.uid;
+    let slug = teamData.team.slug;
+    let location = teamData.team.location;
+    let name = teamData.team.name;
+    let abbreviation = teamData.team.abbreviation;
+    let displayName = teamData.team.displayName;
+    let shortDisplayName = teamData.team.shortDisplayName;
+    let league = newLeague._id;
+    let players = [];
+    const newTeam = new Team({
+      _id: new mongoose.Types.ObjectId(),
+      id,
+      uid,
+      slug,
+      location,
+      name,
+      abbreviation,
+      displayName,
+      shortDisplayName,
+      league,
+      players,
+    });
+    let rosterAbbreviation = getTeamRosterAbbreviation(newTeam.abbreviation);
+    let athletes = await getAthletes('NBA', rosterAbbreviation);
+    async.each(athletes, (playerData, callback) => {
+      let id = playerData.id;
+      let uid = playerData.uid;
+      let guid = playerData.guid;
+      let alternateIds = playerData.alternateIds;
+      let firstName = playerData.firstName;
+      let lastName = playerData.lastName;
+      let fullName = playerData.fullName;
+      let displayName = playerData.displayName;
+      let shortName = playerData.shortName;
+      let weight = playerData.weight;
+      let displayWeight = playerData.displayWeight;
+      let height = playerData.height;
+      let displayHeight = playerData.displayHeight;
+      let age = playerData.age;
+      let dateOfBirth = playerData.dateOfBirth;
+      let debutYear = playerData.debutYear;
+      let links = playerData.links;
+      let birthPlace = playerData.birthPlace;
+      let college = playerData.college;
+      let slug = playerData.slug;
+      let headshot = playerData.headshot;
+      let jersey = playerData.jersey;
+      let hand = playerData.hand;
+      let position = playerData.position;
+      let injuries = playerData.injuries;
+      let contracts = playerData.contracts;
+      let experience = playerData.experience.years;
+      let league = newTeam.league;
+      let team = newTeam._id;
+      let earnings = playerData.earnings;
+      const newPlayer = new Player({
+        id,
+        uid,
+        guid,
+        alternateIds,
+        firstName,
+        lastName,
+        fullName,
+        displayName,
+        shortName,
+        weight,
+        displayWeight,
+        height,
+        displayHeight,
+        age,
+        dateOfBirth,
+        debutYear,
+        links,
+        birthPlace,
+        college,
+        slug,
+        headshot,
+        jersey,
+        hand,
+        position,
+        injuries,
+        contracts,
+        experience,
+        league,
+        team,
+        earnings,
+      });
+      newPlayer.save()
+      .then(() => {
+        newTeam.players.push(newPlayer);
+        callback();
+      })
+    })
+    .then(() => {
+      newTeam.save()
+      .then(() => {
+        newLeague.teams.push(newTeam);
+      })
+    })
+  })
+  return newLeague;
+}
 
-  newLeague.save()
-  .then(() => res.json('League added!'))
-  .catch(err => res.status(400).json('error: ' + err));
+router.route('/').get((req, res) => {
+  League.find()
+    .then(leagues => res.json(leagues))
+    .catch(err => res.status(400).json('error: ' + err));
 });
 
 router.route('/add-all').get(async (req, res) => {
-  let newLeagues = [];
-  for(let league of Object.keys(links)){
-    newLeagues.push(await getLeague(league));
-  }
-  League.collection.insertMany(newLeagues)
+  await League.collection.drop()
+    .catch();
+  await Team.collection.drop()
+    .catch();
+  await Player.collection.drop()
+    .catch();
+  async.forEach(Object.keys(links), (leagueAbbreviation, callback) => {
+    await getLeague(leagueAbbreviation)
+    .then((league) => {
+    let newLeague = await addLeague(league);
+    newLeague.save();
+    callback();
+    });
+  })
   .then(() => res.json('All Leagues Added!'))
   .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/add-nba').get(async (req, res) => {
+router.route('/add/nba').get(async (req, res) => {
   await League.collection.drop()
     .catch();
   await Team.collection.drop()
@@ -52,130 +162,10 @@ router.route('/add-nba').get(async (req, res) => {
     .catch();
   await getLeague('NBA')
   .then((league) => {
-    let id = league.id;
-    let name = league.name;
-    let abbreviation = league.abbreviation;
-    let shortName = league.shortName;
-    let slug = league.slug;
-    let teams = [];
-    const newLeague = new League({
-      _id: new mongoose.Types.ObjectId(),
-      id,
-      name,
-      abbreviation,
-      shortName,
-      slug,
-      teams,
-    });
-    async.each(league.teams, async (teamData) => {
-      let id = teamData.team.id;
-      let uid = teamData.team.uid;
-      let slug = teamData.team.slug;
-      let location = teamData.team.location;
-      let name = teamData.team.name;
-      let abbreviation = teamData.team.abbreviation;
-      let displayName = teamData.team.displayName;
-      let shortDisplayName = teamData.team.shortDisplayName;
-      let league = newLeague._id;
-      let players = [];
-      const newTeam = new Team({
-        _id: new mongoose.Types.ObjectId(),
-        id,
-        uid,
-        slug,
-        location,
-        name,
-        abbreviation,
-        displayName,
-        shortDisplayName,
-        league,
-        players,
-      });
-      let rosterAbbreviation = getTeamRosterAbbreviation(newTeam.abbreviation);
-      let athletes = await getAthletes('NBA', rosterAbbreviation);
-      async.each(athletes, (playerData, callback) => {
-        let id = playerData.id;
-        let uid = playerData.uid;
-        let guid = playerData.guid;
-        let alternateIds = playerData.alternateIds;
-        let firstName = playerData.firstName;
-        let lastName = playerData.lastName;
-        let fullName = playerData.fullName;
-        let displayName = playerData.displayName;
-        let shortName = playerData.shortName;
-        let weight = playerData.weight;
-        let displayWeight = playerData.displayWeight;
-        let height = playerData.height;
-        let displayHeight = playerData.displayHeight;
-        let age = playerData.age;
-        let dateOfBirth = playerData.dateOfBirth;
-        let debutYear = playerData.debutYear;
-        let links = playerData.links;
-        let birthPlace = playerData.birthPlace;
-        let college = playerData.college;
-        let slug = playerData.slug;
-        let headshot = playerData.headshot;
-        let jersey = playerData.jersey;
-        let hand = playerData.hand;
-        let position = playerData.position;
-        let injuries = playerData.injuries;
-        let contracts = playerData.contracts;
-        let experience = playerData.experience.years;
-        let league = newTeam.league;
-        let team = newTeam._id;
-        let earnings = playerData.earnings;
-        const newPlayer = new Player({
-          id,
-          uid,
-          guid,
-          alternateIds,
-          firstName,
-          lastName,
-          fullName,
-          displayName,
-          shortName,
-          weight,
-          displayWeight,
-          height,
-          displayHeight,
-          age,
-          dateOfBirth,
-          debutYear,
-          links,
-          birthPlace,
-          college,
-          slug,
-          headshot,
-          jersey,
-          hand,
-          position,
-          injuries,
-          contracts,
-          experience,
-          league,
-          team,
-          earnings,
-        });
-        newPlayer.save()
-        .then(() => {
-          newTeam.players.push(newPlayer);
-          callback();
-        })
-      })
-      .then(() => {
-        newTeam.save()
-        .then(() => {
-          newLeague.teams.push(newTeam);
-          console.log('2');
-        })
-      })
-    })
-    .then(() => {
-      console.log('3');
-      newLeague.save()
-      .then(() => res.json('NBA League Added!'))
-      .catch(err => res.status(400).json('Error: ' + err));
-    })
+    let newLeague = await addLeague(league);
+    newLeague.save()
+    .then(() => res.json('NBA League Added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
   })
 });
 
@@ -183,13 +173,13 @@ router.route('/:id').get((req, res) => {
   League.findbyid(req.params.id)
   .then(league => res.json(league))
   .catch(err => res.status(400).json('error: ' + err))
-  });
+});
   
 router.route('/:id').delete((req, res) => {
   League.findbyidanddelete(req.params.id)
   .then(() => res.json('League deleted.'))
   .catch(err => res.status(400).json('error: ' + err))
-  });
+});
   
 router.route('/update/:id').post((req, res) => {
   League.findbyid(req.params.id)
@@ -203,6 +193,6 @@ router.route('/update/:id').post((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err))
     })
     .catch(err => res.status(400).json('Error: ' + err))
-  });
+});
 
 module.exports = router;
